8eb9dc8bba86bcab50323338d891beb30c293139
Min kyeong Tak <mtak0235@gmail.com>
2022-07-22 16:30:53 +0900

    fscanf에서form 맞출 때 개행도 넣어야 했다. paper에서 밀 때 height가 아닌 width단위로 밀어야 된다. fscanf의 eof는 -1이다 fscanf 리턴이 폼 갯수랑 맞는지 확인해줘야 한다 문자형 1바이트, int형4바이트

--- mini_paint/mini_mtak.c
+++ mini_paint/mini_mtak.c
@@ -1,24 +1,13 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <math.h>
-
-int ft_strlen(char const *str) {
-	int cnt = 0;
-	while (str[cnt])
-		cnt++;
-	return cnt;	
-}
-int std_error(char const *str) {
-	if (str) 
-		return write(1, str, ft_strlen(str));
-	return 1;
-}
-typedef struct s_setting {
+typedef struct s_paper {
 	int width;
 	int height;
-	char bgc;
-} t_setting;
+	char c;
+} t_paper;
+
 typedef struct s_circle {
 	char t;
 	float cx;
@@ -27,22 +16,34 @@ typedef struct s_circle {
 	char b;
 } t_circle;
 
-int is_right_size(int n) {
+int ft_strlen(char *str) {
+	int cnt = 0;
+	while (str[cnt])
+		cnt++;
+	return cnt;
+}
+
+int ft_err(char *msg) {
+	if (msg)
+		write(1, msg, ft_strlen(msg));
+	return 1;
+}
+
+int is_valid_size(int n) {
 	if (!(0 < n && n <= 300))
 		return 0;
 	return 1;
 }
-
-char *get_setting(FILE *fd, t_setting *set) {
+char *get_paper(FILE *fd, t_paper *paper_info) {
 	char *paper;
-	if (fscanf(fd, "%d %d %c", &set->width, &set->height, &set->bgc))
-		return NULL;
-	if (!(is_right_size(set->width) && is_right_size(set->height)))
-		return NULL;
-	if (!(paper = (char *)malloc(sizeof(char) * (set->width * set->height))))
-		return NULL;
-	for (int i = 0; i < sizeof(set->width * set->height); i++)
-		paper[i] = set->bgc;
+	if (fscanf(fd, "%d %d %c\n", &paper_info->width, &paper_info->height, &paper_info->c) != 3)
+		return 0;
+	if (!is_valid_size(paper_info->width) && !is_valid_size(paper_info->height))
+		return 0;
+	if (!(paper = (char *)malloc(sizeof(char) * (paper_info->height * paper_info->width))))
+		return 0;
+	for (int i = 0; i < (paper_info->width * paper_info->height); i++)
+		paper[i] = paper_info->c;
 	return paper;
 }
 
@@ -52,62 +53,62 @@ int clear(FILE *fd, char *paper, char *msg) {
 	if (paper)
 		free(paper);
 	if (msg)
-		std_error(msg);
+		ft_err(msg);
 	return 1;
 }
 
-int is_in_circle(t_circle *circle, float x, float y) {
-	float diff;
-	if ((diff = sqrtf(powf(x - circle->cx, 2.) + powf(y - circle->cy, 2.))) > circle->r)
+int is_paintable(float x, float y, t_circle *circle) {
+	float distance = sqrtf(powf(circle->cx - x, 2.) + powf(circle->cy -y, 2.));
+	if (distance > circle->r)
 		return 0;
-	if (circle->r - diff < 1.)
-		return 2;
+	if (circle->t == 'C')
+		return 1;
+	if (circle->r - distance > 1.)
+		return 0;
 	return 1;
 }
 
-void draw_circle(char *paper, t_circle *circle, t_setting *setting) {
-	for (int y = 0; y < setting->height; y++) {
-		for (int x = 0; x < setting->width; x++) {
-			if ((circle->t == 'c' && is_in_circle(circle, (float)x, (float)y) == 2) 
-			|| (circle->t == 'C' && is_in_circle(circle, (float)x, (float)y)))
-				paper[y * setting->width + x] = circle->b;
+void draw_circle(char *paper, t_circle *circle, t_paper *pi) {
+	for (int y = 0; y < pi->height; y++) {
+		for (int x = 0; x < pi->width; x++) {
+			if (is_paintable((float)x, (float)y, circle))
+				paper[y * pi->width + x] = circle->b;
 		}
 	}
 }
 
-int draw_circles(FILE *fd, char *paper, t_setting *settings) {
+int draw_circles(char *paper, t_paper *pi, FILE *fd) {
 	t_circle circle;
 	int ret;
-	while ((ret = fscanf(fd, "%c %f %f %f %c", &circle.t, &circle.cx, &circle.cy, &circle.r, &circle.b))) {
-		if (circle.r <= 0.000000000 || (circle.t != 'c' || circle.t != 'C'))
+	while ((ret = fscanf(fd, "%c %f %f %f %c\n", &circle.t, &circle.cx,&circle.cy,&circle.r, &circle.b)) == 5) {
+		if (!(circle.t == 'c' || circle.t == 'C') || circle.r <= 0.)
 			return 0;
-		draw_circle(paper, &circle, settings);
+		draw_circle(paper, &circle, pi);
 	}
-	printf("%d\n", ret);
-	//
-	return 1;
+	return ret == -1? 1 : 0;
 }
 
-void print_pic(char *paper, t_setting *setting) {
-	for (int i = 0; i < setting->height; i++) {
-		write(1, paper + i * setting->width, setting->width);
-		write(1, "\n", 1);
+void print_paper(char *paper, t_paper *pi) {
+	for (int i = 0; i < pi->height; i++) {
+		write(1, &paper[i * pi->width], pi->width);
+		write(1,"\n", 1);
 	}
 }
-int main(int arg, char **av) {
-	t_setting setting;
+
+int main(int ag, char **av) {
+	FILE *fd;
+	t_paper paper_info;
 	char *paper;
 
-	if (arg != 2)
-		return std_error("Error: argument\n");
-	FILE *fd;
-	if (!(fd = fopen(av[1], "r")))
-		return std_error("Error: Operation file corrupted 1\n");
-	if (!(paper = get_setting(fd, &setting)))
-		return clear(fd, NULL, "Error: Operation file corrupted 2\n");
-	if (!draw_circles(fd, paper, &setting)) 
-		return clear(fd, paper, "Error: Operation file corrupted 3\n");
-	print_pic(paper, &setting);
+	if (ag !=  2)
+		return ft_err("Error: argument\n");
+	if (!(fd = fopen(av[1],"r")))
+		return ft_err("Error: Operation file corrupted\n");
+	if (!(paper = get_paper(fd, &paper_info)))
+		return clear(fd, NULL, "Error: Operation file corrupted\n");
+	if (!draw_circles(paper, &paper_info, fd))
+		return clear(fd, paper, "Error: Operation file corrupted\n");
+	print_paper(paper, &paper_info);
 	clear(fd, paper, NULL);
 	return 0;
 }
